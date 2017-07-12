746c0d8834f6e8d355caf16a5d6afd8f2c24bbf2
jianjianer <670386323@qq.com>
2017-07-12 10:18:51 +0800

    搭建个人Blog框架
    使用C# ASP.NET MVC搭建个人Blog

---
+++ Auth/AuthAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Auth
+{
+    public class AuthAttribute:ActionFilterAttribute
+    {
+        public int iss = 1;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+         {
+            base.OnActionExecuting(filterContext); 
+            
+            string requestedWith = filterContext.RequestContext.HttpContext.Request.Params["HTTP_X_REQUESTED_WITH"];
+            if (!string.IsNullOrEmpty(requestedWith) && requestedWith.Equals("XMLHttpRequest")) {
+                filterContext.HttpContext.Response.Headers.Set("sessionstatus","timeout");
+            }  
+            var user = filterContext.RequestContext.HttpContext.Session["user"];
+
+            //if (count % 2 == 0)
+            //{
+            //    var session = filterContext.HttpContext.Session["user"];
+            //    filterContext.HttpContext.Response.Redirect("~/Account/Login", true);
+            //    filterContext.HttpContext.Response.End();
+            //    filterContext.Result = new ContentResult();
+            //    return;
+            //}
+            //else {
+            //    filterContext.HttpContext.Session["user"] = "唐健";
+            //}
+            filterContext.HttpContext.Session["user"] = "阿凡达";
+            
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Test.Auth;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            HttpContext.Session["user"] = "唐健";
+            return View();
+        }
+         
+        [HttpPost]
+        [AuthAttribute]
+        public ContentResult GetData(string json,int count)
+        { 
+            return Content("尖子");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
